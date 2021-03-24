97e8ebc08c31526f3db8512899f41c609d832483
DiosAkhilleus <abertin1809@gmail.com>
2021-03-23 23:15:34 -0500

    minor cleanup

--- src/components/Translation.js
+++ src/components/Translation.js
@@ -16,10 +16,10 @@ const Translation = (props) => {
             <h2 className="title">{props.provided}</h2> 
             <div className="type">{props.type}</div><br/>
             <div className="head">{props.head}</div><br/>
+            <div className="short">{props.short}</div><br/>
             <div>{(props.provided !== '') ? '––––––––––––––––––––' : ''}</div>
             <div>{props.inflections.map((item, i) => format(item, i))}</div>
             <div>{(props.provided !== '') ? '––––––––––––––––––––' : ''}</div><br/>
-            <div className="short">{props.short}</div> <br/>
             <div className="long">{props.long}</div>
             <br/>
             <br/>
