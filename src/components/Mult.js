6945ec88df547b59586e8a9530a30c3b866115ec
DiosAkhilleus <abertin1809@gmail.com>
2021-06-02 23:05:15 -0500

    cleaned up component a bit

--- src/components/Mult.js
+++ src/components/Mult.js
@@ -4,16 +4,17 @@ const Mult = ({ input, provided, lang }) => {
     // maps through the input array and provides a Translation component from the information inside input
     return (
         <>
-        {/* {console.log(props.input, props.provided, props.lang)} */}
-        {input.map((el, index) => {return ( 
-            <Translation key={Math.random()}
-          provided={provided}
-          head={`From: ${el.headword}`} 
-          type={el.type.charAt(0).toUpperCase() + el.type.slice(1)} 
-          inflections={el.inflections} 
-          short={`Wiki Definition: ${el.shortDef}`} 
-          long={(el.lang === 'la') ? `Lewis & Short Entry: ${el.longDef}` : `Liddell Scott Entry: ${el.longDef}`} />
-         )})}
+            {input.map((el, index) => {return ( 
+                <Translation 
+                    key={Math.random()}
+                    provided={provided}
+                    head={`From: ${el.headword}`} 
+                    type={el.type.charAt(0).toUpperCase() + el.type.slice(1)} 
+                    inflections={el.inflections} 
+                    short={`Wiki Definition: ${el.shortDef}`} 
+                    long={(el.lang === 'la') ? `Lewis & Short Entry: ${el.longDef}` : `Liddell Scott Entry: ${el.longDef}`} 
+                />
+            )})}
         </>
     )
 }
